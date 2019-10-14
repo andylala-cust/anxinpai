8cfd421ad9580010f6ff41b597988df540edcd00
andylala <857511368@qq.com>
2019-10-14 15:46:36 +0800

    房源详情完善

---
+++ js/page/HouseValue.js
@@ -0,0 +1,63 @@
+import React,{Component} from 'react';
+import {View,Text} from 'react-native';
+import {WebView} from 'react-native-webview';
+import {HeaderButton, HeaderButtons, Item} from 'react-navigation-header-buttons';
+import {STATUSBAR_HEIGHT} from '../util';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import {connect} from 'react-redux';
+
+// 获取状态栏高度，只对安卓有效，iphone是20, iphonex是44
+const STATUSBAR_STYLE = Platform.OS === 'ios' ? {} : {
+  height: 2*STATUSBAR_HEIGHT+15,
+  paddingTop: STATUSBAR_HEIGHT
+}
+const IoniconsHeaderButton = passMeFurther => (
+  // the `passMeFurther` variable here contains props from <Item .../> as well as <HeaderButtons ... />
+  // and it is important to pass those props to `HeaderButton`
+  // then you may add some information like icon size or color (if you use icons)
+  <HeaderButton {...passMeFurther} IconComponent={Ionicons} iconSize={26} color="#000" />
+);
+
+let self;
+
+class HouseValue extends Component {
+  static navigationOptions = {
+    title: '房产价值',
+    // 解决安卓标题不居中
+    headerTitleStyle:{
+      flex: 1,
+      textAlign:'center'
+    },
+    headerLeft: (
+      <HeaderButtons HeaderButtonComponent={IoniconsHeaderButton}>
+        <Item title="back" iconName="ios-arrow-back" onPress={() => self.btnPress()} />
+      </HeaderButtons>
+    ),
+    // 解决左边有按钮，右边无按钮标题不居中
+    headerRight: <View />,
+    // 安卓沉浸式状态栏适配
+    headerStyle: STATUSBAR_STYLE
+  }
+  constructor (props) {
+    super(props)
+    self = this
+  }
+  btnPress () {
+    this.props.navigation.goBack()
+  }
+  render () {
+    return (
+      <WebView
+        startInLoadingState={true}
+        style={{flex: 1}}
+        source={{uri: this.props.houseValueUrl}}
+      />
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  houseValueUrl: state.houseInfo.houseValueUrl
+})
+
+export default connect(mapStateToProps)(HouseValue);
---
+++ js/page/index.js
@@ -0,0 +1,19 @@
+import Info from './Info';
+import Detail from './Detail';
+import More from './More';
+import HouseInfo from './HouseInfo';
+import Demo from './Demo';
+import Court from  './Court';
+import Around from './Around';
+import HouseValue from './HouseValue';
+
+export {
+  Info,
+  Detail,
+  More,
+  HouseInfo,
+  Demo,
+  Court,
+  Around,
+  HouseValue
+}
